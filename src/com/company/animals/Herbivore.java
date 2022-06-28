52d774c26c9723427a73c91c18a45701c5050f22
HellEnjoy <101444928+HellEnjoy@users.noreply.github.com>
2022-06-28 17:36:26 +0600

    Homework of two(ver 0.0)

--- src/com/company/animals/Herbivore.java
+++
@@ -1,6 +0,0 @@
-package com.company.animals;
-
-public interface Herbivore extends Animal {
-    @Override
-    void Action();
-}
--- src/com/company/animals/Predator.java
+++
@@ -1,6 +0,0 @@
-package com.company.animals;
-
-public interface Predator extends Animal{
-    @Override
-    void Action();
-}
